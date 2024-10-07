a7c5c8da273bab956c200a5bd03c1c88072f3591
jay <jayhp0410@gmail.com>
2024-10-07 10:32:35 +0530

    Add util files

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+
+const app = express();
+
+app.use(cors({
+    origin : process.env.CORS_ORIGIN,
+    credentials : true
+}))
+
+app.use(express.json({limit : "16kb"})) // configration of json data access
+app.use(express.urlencoded({extended : true, limit :"16kb"})) //configration for url
+app.use(express.static("public")) // configration for 
+
+app.use(cookieParser()) //configration for cookies -> browser and server manage or send and recive cookies 
+
+export {app};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import dotenv from "dotenv";
 dotenv.config({path : " ./env"})
 import connectDb from "./db/db.js";
+import { app } from "./app.js";
 
-connectDb();
+connectDb()
+.then(()=>{
+    
+    /* app.on("Error",(err)=>{
+        console.log("Error", err);
+        throw err;
+    })*/
+
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running successfully on port no : ${process.env.port}`);
+    })
+})
+.catch((err) => {
+    console.log("Database connection fail!!!" , err);
+})
